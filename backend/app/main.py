3fa0986020111af67e8e54dadc359923eecf4f10
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2026-03-27 15:02:48 -0400

    chore: remove lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from app.database import init_db
 from app.api.main import api_router
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-   init_db()
-   yield
-   print("App Finalizada")
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 @app.get('/')
