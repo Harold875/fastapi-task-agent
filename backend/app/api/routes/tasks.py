6049b42989cf6083667528bd2a08a5356b9d28d8
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2026-03-30 13:37:34 -0400

    feat: add task summary endpoint

--- backend/app/api/routes/tasks.py
+++ backend/app/api/routes/tasks.py
@@ -1,12 +1,14 @@
-from datetime import date
+from datetime import date, timedelta, timezone
 from typing import Annotated
+from collections import defaultdict
 
 from fastapi import APIRouter, HTTPException, Query, status
 from sqlalchemy import select
 
 from app.api.deps import SessionDB
-from app.models import Task, TaskStatus, TaskPriority
-from app.schema import TaskBase, TaskPublic, TaskUpdate
+from app.models import Task, TaskStatus, TaskPriority, get_datetime_utc
+from app.schema import TaskBase, TaskPublic, TaskUpdate, TaskSummary
+from app.agent.task_agent import MyDeps, agent
 
 router = APIRouter(prefix='/tasks', tags=['tasks'])
 
@@ -104,3 +106,62 @@ async def delete_task(session: SessionDB, id: int) -> None:
     session.delete(task)
     session.commit() 
     return
+
+
+@router.get("/summary/")
+async def summary_day(
+    session: SessionDB,
+) -> TaskSummary:
+    stmt = select(Task)
+    tasks = session.scalars(stmt).all()
+
+    current_date = get_datetime_utc()
+    upcommit_due_date = current_date + timedelta(days=3)
+    
+    total_tasks = len(tasks)
+    dist_by_status = defaultdict(int)
+    dist_by_priority = defaultdict(int)
+    
+    tasks_overdue = 0
+    tasks_due_soon = 0
+    for task in tasks:
+        # distribution by status and priority
+        dist_by_status[task.status] += 1
+        dist_by_priority[task.priority] += 1
+        
+        t_due_date = task.due_date.replace(tzinfo=timezone.utc)
+        
+        # overdue and upcoming tasks
+        if t_due_date < current_date:
+            tasks_overdue += 1
+        elif t_due_date < upcommit_due_date:
+            tasks_due_soon += 1
+    
+    
+    response = {
+        "total_tasks": total_tasks,
+        "distribution_by_status": dist_by_status,
+        "distribution_by_priority": dist_by_priority,
+        "tasks_overdue": tasks_overdue,
+        "tasks_due_soon": tasks_due_soon,
+    }
+    
+    # AI
+    deps = MyDeps(session=session)
+    try:
+        result = await agent.run(
+            (
+                "Dame una sugerencia de cual tarea deberia hacer"
+                "enfocada en la prioridad para este dia.\n\n"
+                "Aqui tienes la estadista sobre las tareas:\n"
+                f"{response}"
+            ),
+            deps=deps,
+        )
+        message_ai = result.output
+    except Exception:
+        message_ai = "ERROR: Something went wrong with the AI agent."
+    
+    response["message"] = message_ai
+    
+    return response
--- backend/app/schema.py
+++ backend/app/schema.py
@@ -22,3 +22,27 @@ class TaskPublic(TaskBase):
 
 class TaskUpdate(TaskBase):
     title: str | None = None
+
+
+class TaskSummary(BaseModel):
+    total_tasks: int
+    distribution_by_status: dict[TaskStatus, int]
+    distribution_by_priority: dict[TaskPriority, int]
+    tasks_overdue: int
+    tasks_due_soon: int
+    message: str
+    
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "total_tasks": 6,
+                    "distribution_by_status": {i.value: 1 for i in TaskStatus},
+                    "distribution_by_priority": {i.value: 1 for i in TaskPriority},
+                    "tasks_overdue": 1,
+                    "tasks_due_soon": 2,
+                    "message": "string",
+                },
+            ]
+        }
+    }
